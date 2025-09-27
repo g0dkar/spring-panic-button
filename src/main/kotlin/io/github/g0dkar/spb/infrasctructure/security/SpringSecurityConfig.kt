f16d9032acc11b333c581edcb7316981caf4b1c0
Rafael Lins <rafael.lins777@gmail.com>
2025-09-27 11:26:27 -0300

    feat(init): Getting good

--- src/main/kotlin/io/github/g0dkar/spb/infrasctructure/security/SpringSecurityConfig.kt
+++ src/main/kotlin/io/github/g0dkar/spb/infrasctructure/security/SpringSecurityConfig.kt
@@ -1,5 +1,6 @@
 package io.github.g0dkar.spb.infrasctructure.security
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -8,7 +9,8 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 class SpringSecurityConfig {
     @Bean
-    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain =
+    @ConditionalOnProperty("panic-button.security.enabled", havingValue = "false", matchIfMissing = true)
+    fun disabledSecurityConfiguration(http: HttpSecurity): SecurityFilterChain =
         http
             .csrf { csrf -> csrf.disable() }
             .build()
